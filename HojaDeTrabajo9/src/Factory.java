b9c2eddaea4fdae8e615788c79e8e47edff03f35
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-04-22 18:09:09 -0600

    factory

---
+++ HojaDeTrabajo9/src/Factory.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+/**
+ *
+ * @author Juan Marroquin
+ */
+public class Factory {
+     public Factory() {
+    }
+    public static Map<String, String[]> obtenerMap(String opcion){
+        switch (opcion){
+            case "1":
+                return new HashMap<>();
+            case "2":
+                //return new HashMap<>();
+                //poner el splay tree
+           
+            default:
+                return null;
+        }
+    }
+}
